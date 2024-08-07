b722e1a35eb8bbff9f269e34edb241c15e4e0b22
进步 <123805832+19142678907@users.noreply.github.com>
2024-08-07 23:20:24 +0800

    Update recover_vless.py

--- vless/recover_vless.py
+++ vless/recover_vless.py
@@ -1,29 +1,46 @@
 import os
 import requests
-import subprocess
+import json
+from subprocess import call
+
+# 环境变量
+ACCOUNTS_JSON = os.getenv('ACCOUNTS_JSON')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 def send_telegram_message(message):
-    telegram_token = os.getenv('TELEGRAM_TOKEN')
-    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    
-    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
-    payload = {
-        'chat_id': telegram_chat_id,
-        'text': message
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': message,
+        'parse_mode': 'Markdown'
     }
-    response = requests.post(url, data=payload)
-    
+    response = requests.post(url, data=data)
     if response.status_code != 200:
-        raise Exception(f"Failed to send message: {response.text}")
+        print(f"Telegram消息发送失败: {response.text}")
+    else:
+        print(f"Telegram消息发送成功: {message}")
 
-def recover_vless():
-    try:
-        result = subprocess.run(['bash', 'vless/check_vless.sh'], check=True, text=True, capture_output=True)
-        output = result.stdout
-        message = f"serv00-vless 恢复操作结果：\n\n{output}"
+def check_vless_status():
+    result = call(["pm2", "status", "vless"])
+    return result == 0
+
+def restart_vless():
+    result = call(["pm2", "restart", "vless"])
+    return result == 0
+
+def main():
+    if not check_vless_status():
+        message = "VLESS 进程未运行，尝试重启..."
         send_telegram_message(message)
-    except subprocess.CalledProcessError as e:
-        send_telegram_message(f"VLESS 服务恢复失败：{e.output}")
+        if restart_vless():
+            message = "VLESS 进程已成功重启。"
+        else:
+            message = "VLESS 进程重启失败。"
+        send_telegram_message(message)
+    else:
+        message = "VLESS 进程正在正常运行。"
+        send_telegram_message(message)
 
 if __name__ == "__main__":
-    recover_vless()
+    main()
