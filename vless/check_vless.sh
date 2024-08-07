d05c7713f8fc1eb6f07b7d421fcab0d5f5591589
进步 <123805832+19142678907@users.noreply.github.com>
2024-08-07 23:20:03 +0800

    Update check_vless.sh

--- vless/check_vless.sh
+++ vless/check_vless.sh
@@ -6,7 +6,21 @@ NC='\033[0m'  # 恢复默认颜色
 
 # 输出绿色的 "YI XIU"
 output_yi_xiu() {
-    echo -e "${GREEN}YI XIU${NC}"
+    echo -e "${GREEN} .----------------.  .----------------.  .----------------.  .----------------.  .----------------. "
+    echo -e "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |"
+    echo -e "| |  ____  ____  | || |     _____    | || |  ____  ____  | || |     _____    | || | _____  _____ | |"
+    echo -e "| | |_  _||_  _| | || |    |_   _|   | || | |_  _||_  _| | || |    |_   _|   | || ||_   _||_   _|| |"
+    echo -e "| |   \\ \\  / /   | || |      | |     | || |   \\ \\  / /   | || |      | |     | || |  | |    | |  | |"
+    echo -e "| |    \\ \\/ /    | || |      | |     | || |    > \`' <    | || |      | |     | || |  | '    ' |  | |"
+    echo -e "| |    _|  |_    | || |     _| |_    | || |  _/ /'\\ \\_  | || |     _| |_    | || |   \\ \`--' /   | |"
+    echo -e "| |   |______|   | || |    |_____|   | || | |____||____| | || |    |_____|   | || |    \`.__.'    | |"
+    echo -e "| |              | || |              | || |              | || |              | || |              | |"
+    echo -e "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |"
+    echo -e " '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ${NC}"
+    echo -e "--------------------------------------------------------------------------------------------------"
+    echo -e "一休YouTuBe: ${GREEN}https://www.youtube.com/@yixiu001${NC}"
+    echo -e "TG技术交流群: ${GREEN}https://t.me/yxjsjl${NC}"
+    echo -e "--------------------------------------------------------------------------------------------------"
 }
 
 # Function to generate a UUID
@@ -18,6 +32,22 @@ generate_uuid() {
             return
         fi
     done
+
+    # 预定义的UUID列表
+    predefined_uuids=(
+        "fb210b24-46dd-4b4c-92ce-097385945dad"
+        "53cfcb07-8c25-4c25-baaa-95b4b50871a2"
+        "445ae56f-727d-495e-9c88-cbe942d144a6"
+        "078eb39d-2094-4272-b221-782ba0520dd6"
+        "5826e9cc-c5b7-49ca-8b37-a0ea68f382cc"
+        "e79fda4a-9519-4ef3-8973-130801b3d0ae"
+        "c0422b3b-00aa-4dbe-8573-6fb15d49e557"
+        "907e3ac9-02de-47fe-b40c-c2bd912c3adf"
+        "c53ca34c-8d9c-4a7e-8b44-5da52e4b5eaa"
+        "73fc0a2d-2458-435b-92aa-b4e8e3e40944"
+    )
+    uuid=${predefined_uuids[$RANDOM % ${#predefined_uuids[@]}]}
+    echo "$uuid"
 }
 
 # Function to save config.json
@@ -33,6 +63,7 @@ save_config() {
 EOL
         echo "生成config.json文件。"
     else
+        # Update the port in config.json if it exists
         jq --arg port "$port" '.port = ($port | tonumber)' ~/domains/$USER.serv00.net/vless/config.json > ~/domains/$USER.serv00.net/vless/config_tmp.json && mv ~/domains/$USER.serv00.net/vless/config_tmp.json ~/domains/$USER.serv00.net/vless/config.json
         echo "config.json文件已存在，端口号已更新。"
     fi
@@ -41,14 +72,21 @@ EOL
 # Function to deploy vless
 deploy_vless() {
     local port=${1:-3000}  # Default port is 3000 if not provided
+    # 修改端口号
     save_config "$port"
+    # 安装依赖
     npm install
+    # 启动vless项目
     ~/.npm-global/bin/pm2 start ~/domains/$USER.serv00.net/vless/app.js --name vless
+    # 保存pm2进程状态
     ~/.npm-global/bin/pm2 save
+    # ANSI颜色码
     output_yi_xiu
     echo -e "端口号: ${GREEN}${port}${NC}"
     echo -e "UUID: ${GREEN}${uuid}${NC}"
     echo -e "域名: ${GREEN}$USER.serv00.net${NC}"
+    echo -e "vless进程维护定时任务脚本: ${GREEN}cd ~/domains/$USER.serv00.net/vless && ./check_vless.sh${NC}"
+    echo -e "VLESS节点信息: ${GREEN}vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless${NC}"
 }
 
 # 启动pm2 vless进程
@@ -116,15 +154,21 @@ main() {
         deploy_vless "$port"
     else
         echo "没有提供-p参数，跳过vless安装。"
+        # 读取 config.json 中的 uuid 和 port
         if [[ -f config.json ]]; then
             uuid=$(jq -r '.uuid' config.json)
             port=$(jq -r '.port' config.json)
             echo -e "UUID: ${uuid}"
             echo -e "Port: ${port}"
             echo -e "域名: $USER.serv00.net"
-            echo -e "VLESS节点信息: vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=%2F&serviceName=&sni=$USER.serv00.net"
+            echo -e "VLESS节点信息: vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless"
+        else
+            echo -e "config.json 文件不存在或格式错误。"
         fi
+        echo "开始检查pm2 vless进程..."
+        check_pm2_vless_status
     fi
 }
 
+# 执行主函数
 main "$@"
